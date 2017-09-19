065a4e364a6025279e164634ce73f43008a69e8f
wcrozier12 <wcc8@nau.edu>
2017-09-19 11:22:40 -0700

    added package.json

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'B1Oa2huOvkIGN9jj2htAPJRC6',
+  consumer_secret: 'MRhD6RATBmaFtApDvBs7HXgfS80gNNJfenfnLbYB5tWTg0LbWl',
+  access_token_key: '909820561324249088-51gZQAWuJQ8GigIJJUbT9ZLpT1TMJFM',
+  access_token_secret: 'Yi2UJ1HKgDdwgyPIYux0TsCT28xrSrBaJ70qzFKJ45t1C',
+}
+
+var spotifyKeys = {
+  client_id: '80e62b314dac472bae3b72015e724401',
+  client_secret: '0593ecf4af704e128bb957b03551e386',
+}
+
+module.exports = {
+  twitterKeys,
+  spotifyKeys,
+};
---
+++ liri.js
@@ -0,0 +1,7 @@
+var keys = require('./keys.js');
+
+var twitterKeys = keys.twitterKeys;
+var spotifyKeys = keys.spotifyKeys;
+
+var action = process.argv[2];
+console.log(action);
